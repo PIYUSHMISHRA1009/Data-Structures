d8300a44f0693e015a8f2d85e3dfec25ee58e696
PIYUSHMISHRA1009 <155824301+PIYUSHMISHRA1009@users.noreply.github.com>
2024-10-20 20:05:08 +0530

    Update additionoftwopolynomialsusingdoublylinkedlist.cpp

--- Week9-11/Week9/additionoftwopolynomialsusingdoublylinkedlist.cpp
+++ Week9-11/Week9/additionoftwopolynomialsusingdoublylinkedlist.cpp
@@ -2,149 +2,102 @@
 using namespace std;
 
 struct Node {
-    int coefficient;
-    int exponent;
+    int coefficient, exponent;
     Node* prev;
     Node* next;
+    Node(int coeff, int exp) : coefficient(coeff), exponent(exp), prev(nullptr), next(nullptr) {}
 };
 
 class Polynomial {
     Node* head;
 
 public:
-    Polynomial() {
-        head = NULL;
-    }
+    Polynomial() : head(nullptr) {}
 
     ~Polynomial() {
-        while (head != NULL) {
-            deleteTerm(0);
-        }
+        while (head) deleteTerm(head->exponent);
     }
 
     void insertTerm(int coefficient, int exponent) {
-        Node* newNode = new Node();
-        newNode->coefficient = coefficient;
-        newNode->exponent = exponent;
-        newNode->next = NULL;
+        if (coefficient == 0) return;
 
-        if (head == NULL || head->exponent < exponent) {
-            newNode->prev = NULL;
-            if (head != NULL) {
-                newNode->next = head;
-                head->prev = newNode;
-            }
+        Node* newNode = new Node(coefficient, exponent);
+        if (!head || head->exponent < exponent) {
+            newNode->next = head;
+            if (head) head->prev = newNode;
             head = newNode;
             return;
         }
 
         Node* temp = head;
-        while (temp->next != NULL && temp->next->exponent > exponent) {
-            temp = temp->next;
-        }
+        while (temp->next && temp->next->exponent > exponent) temp = temp->next;
 
         if (temp->exponent == exponent) {
             temp->coefficient += coefficient;
-            delete newNode;
-            if (temp->coefficient == 0) {
-                deleteTerm(temp->exponent);
-            }
+            delete newNode; 
+            if (temp->coefficient == 0) deleteTerm(temp->exponent);
             return;
         }
 
         newNode->prev = temp;
         newNode->next = temp->next;
-        if (temp->next != NULL) {
-            temp->next->prev = newNode;
-        }
+        if (temp->next) temp->next->prev = newNode;
         temp->next = newNode;
     }
 
     void deleteTerm(int exponent) {
-        if (head == NULL) return;
-
         Node* temp = head;
-        while (temp != NULL && temp->exponent != exponent) {
-            temp = temp->next;
-        }
-        if (temp == NULL) return;
 
-        if (temp->prev != NULL) {
-            temp->prev->next = temp->next;
-        } else {
-            head = temp->next;
-        }
-        if (temp->next != NULL) {
-            temp->next->prev = temp->prev;
-        }
-        delete temp;
+        while (temp && temp->exponent != exponent) temp = temp->next;
+        
+        if (!temp) return; 
+
+        if (temp->prev) temp->prev->next = temp->next; 
+        else head = temp->next; 
+        
+        if (temp->next) temp->next->prev = temp->prev; 
+        delete temp; 
     }
 
-    Polynomial add(const Polynomial& other) {
+    Polynomial add(const Polynomial& other) const {
         Polynomial result;
-        Node* p1 = head;
-        Node* p2 = other.head;
-
-        while (p1 != NULL || p2 != NULL) {
-            if (p1 != NULL && (p2 == NULL || p1->exponent > p2->exponent)) {
-                result.insertTerm(p1->coefficient, p1->exponent);
-                p1 = p1->next;
-            } else if (p2 != NULL && (p1 == NULL || p2->exponent > p1->exponent)) {
-                result.insertTerm(p2->coefficient, p2->exponent);
-                p2 = p2->next;
-            } else {
-                result.insertTerm(p1->coefficient + p2->coefficient, p1->exponent);
-                p1 = p1->next;
-                p2 = p2->next;
-            }
+        for (Node *p1 = head, *p2 = other.head; p1 || p2;) {
+            if (!p1 || (p2 && p2->exponent > p1->exponent)) 
+                result.insertTerm(p2->coefficient, p2->exponent), p2 = p2->next;
+            else if (!p2 || (p1 && p1->exponent > p2->exponent)) 
+                result.insertTerm(p1->coefficient, p1->exponent), p1 = p1->next;
+            else 
+                result.insertTerm(p1->coefficient + p2->coefficient, p1->exponent), p1 = p1->next, p2 = p2->next;
         }
         return result;
     }
 
-    void printPolynomial() {
-        Node* temp = head;
-        while (temp != NULL) {
-            cout << temp->coefficient << "x^" << temp->exponent;
-            if (temp->next != NULL) {
-                cout << " + ";
-            }
-            temp = temp->next;
+    void printPolynomial() const {
+        for (Node* temp = head; temp; temp = temp->next) {
+            cout << temp->coefficient << "x^" << temp->exponent << (temp->next ? " + " : "");
         }
         cout << endl;
     }
 };
 
 int main() {
-    Polynomial poly1, poly2, result;
-    int choice, coefficient, exponent;
+    Polynomial poly1, poly2;
 
-    cout << "Polynomial A:\n";
-    while (true) {
-        cout << "Enter coefficient and exponent (or -1 to finish): ";
-        cin >> coefficient;
-        if (coefficient == -1) break;
-        cin >> exponent;
-        poly1.insertTerm(coefficient, exponent);
+    for (int coeff, exp; true;) {
+        cout << "Polynomial A: Enter coefficient and exponent (-1 to finish): ";
+        cin >> coeff; if (coeff == -1) break; cin >> exp; poly1.insertTerm(coeff, exp);
     }
 
-    cout << "Polynomial B:\n";
-    while (true) {
-        cout << "Enter coefficient and exponent (or -1 to finish): ";
-        cin >> coefficient;
-        if (coefficient == -1) break;
-        cin >> exponent;
-        poly2.insertTerm(coefficient, exponent);
+    for (int coeff, exp; true;) {
+        cout << "Polynomial B: Enter coefficient and exponent (-1 to finish): ";
+        cin >> coeff; if (coeff == -1) break; cin >> exp; poly2.insertTerm(coeff, exp);
     }
 
-    result = poly1.add(poly2);
-
-    cout << "Polynomial A: ";
-    poly1.printPolynomial();
-    cout << "Polynomial B: ";
-    poly2.printPolynomial();
-    cout << "Result of A + B: ";
-    result.printPolynomial();
+    Polynomial result = poly1.add(poly2);
+    
+    cout << "Polynomial A: "; poly1.printPolynomial();
+    cout << "Polynomial B: "; poly2.printPolynomial();
+    cout << "Result of A + B: "; result.printPolynomial();
 
     return 0;
 }
-
